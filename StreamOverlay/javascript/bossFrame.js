e1f57881f15818dd7fe9d076b1d57bc779d6a499
Adam <73717269+adamabad@users.noreply.github.com>
2022-03-29 00:08:40 -0400

    Code Cleanup / Added interact.js

--- StreamOverlay/javascript/bossFrame.js
+++ StreamOverlay/javascript/bossFrame.js
@@ -1,18 +1,4 @@
-window.onload = function () {
-    let bar = document.querySelectorAll('.bar');
-    bar.forEach((progress) => {
-      let value = progress.getAttribute('data-value');
-      progress.style.width = `${value}%`;
-      let count = 0;
-      let progressAnimation = setInterval(() => {
-        count++;
-        progress.setAttribute('data-text', `${count}%`);
-        if (count >= value) {
-          clearInterval(progressAnimation);
-        }
-      }, 15);
-    });
-  };
+
 
 function updateBar(value, currentBest) {
     let bar = document.querySelectorAll('.bar');
---
+++ StreamOverlay/javascript/general.js
@@ -0,0 +1,38 @@
+//https://interactjs.io/docs/draggable/
+const position = { x: 0, y: 0 }
+var locked = true;
+
+interact('.draggable').draggable({
+  listeners: {
+    start (event) {
+      console.log(event.type, event.target)
+    },
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+})
+
+function locktoggle() 
+{
+  let toggleable = document.querySelectorAll('.draggable-target');
+
+  if(locked) 
+  {
+    locked = false;
+    toggleable.forEach((e) => {
+      e.classList.add("draggable");
+    });
+  }
+  else
+  {
+    locked = true;
+    toggleable.forEach((e) => {
+      e.classList.remove("draggable");
+    });
+  }
+}
