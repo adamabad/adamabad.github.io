a6f0d6235b071525ea6d06a0a087c1d843033313
Adam <73717269+adamabad@users.noreply.github.com>
2022-04-01 00:40:43 -0400

    Big timer logic push +misc

--- StreamOverlay/javascript/timerFrame.js
+++ StreamOverlay/javascript/timerFrame.js
@@ -6,37 +6,39 @@ var cursplit = 0;
 var display = document.getElementById('display');
 var times = document.getElementById('times');
 var table = document.getElementById('times');
-var split_element = [];
+var previous_split_timer = document.getElementById('previous_time');
+var split_delta;
+var split_timer;
+var run_delta = null;
+var delta_time;
+var pb;
 
 var dungeon_splits = [
-    ["", "Pack 1", "", "1:40"], 
-    ["", "Boss 1", "", "3:10"], 
-    ["", "Pack 2 - 3", "", "4:53"], 
-    ["", "Boss 2", "", "6:45"],
-    ["", "Pack 4 - 5", "", "8:56"],
-    ["", "Boss 3", "", "11:01"]
+    ["", "Pack 1", "", 10000], 
+    ["", "Boss 1", "", 19000], 
+    ["", "Pack 2 - 3", "", 29400], 
+    ["", "Boss 2", "", 40500],
+    ["", "Pack 4 - 5", "", 53600],
+    ["", "Boss 3", "", 66100]
 ]
-var L = dungeon_splits.length;
+var split_List = dungeon_splits;
+var L = split_List.length;
 
 window.onload = () => {
     
     for(x = 0; x < (L - 1); x++) {
-        let element = dungeon_splits[x];
-        var localArray = [];
+        let element = split_List[x];
         table.innerHTML += `<div class="table_row">`
         + ` <div class="split_img"><img src=${element[0]}></img></div>`
         + ` <div class="split_title">${element[1]}</div>`
         + ` <div class="split_delta" id="d${x}" data-value="">${element[2]}</div>`
-        + ` <div class="split_time" id="id${x}">${element[3]}</div>`
-        + `</div>`
-        split_element[x] = [document.getElementById(`d${x}`), document.getElementById(`id${x}`)];
+        + ` <div class="split_time" id="id${x}">${toTime(element[3])}</div>`
+        + `</div>` 
     }
-    document.getElementById('final_split_img').setAttribute('src', dungeon_splits[L - 1][0]);
-    document.getElementById('final_split_title').innerHTML = dungeon_splits[L - 1][1];
-    document.getElementById('final_split_delta').innerHTML = dungeon_splits[L - 1][2];
-    document.getElementById('final_split_time').innerHTML = dungeon_splits[L - 1][3];
-
-    console.log(split_element);
+    document.getElementById('final_split_img').setAttribute('src', split_List[L - 1][0]);
+    document.getElementById('final_split_title').innerHTML = split_List[L - 1][1];
+    document.getElementById('final_split_delta').innerHTML = split_List[L - 1][2];
+    document.getElementById('final_split_time').innerHTML = toTime(split_List[L - 1][3]);
 };
 
 function cleanUI() {
@@ -55,36 +57,35 @@ function startStopwatch() {
         return;
 
     start = new Date();
-
-    if(split) {
-        times.style.display = 'none';
-        times.innerHTML = '';
-    
-        split = null;
-    }
-
+    split = start;
+    pb = split_List[cursplit][3];
     function tick() {
         var now = new Date();
-        
-        if(split) {
-            var html = '<div class="total-time">' 
-                + renderTime(start, now) 
-                + '</div>'
-                + '<div class="split-time">' 
-                + renderTime(split, now)
-                + '</div>';
-        
-            display.innerHTML = html;
-        } else {
-            var html = '<div class="total-time">' 
-                + renderTime(start, now) 
-                + '</div>';
-        
-            display.innerHTML = html;
+        delta_time = (now - start) -  split_List[cursplit][3];
+        delta = (now - start) - pb;
+        if(run_delta == null && delta_time > -10000) {
+            split_delta.innerHTML = renderDeltaTime(delta_time);
+            if(delta_time < 0) {
+                split_delta.setAttribute('style', 'color:green');
+            }
+            else {
+                split_delta.setAttribute('style', 'color:red');
+            }
         }
+        else if(delta_time > -10000 || delta_time > run_delta) {
+            split_delta.innerHTML = renderDeltaTime(delta_time);
+            colorGen(split_delta);
+        }
+        var html = '<div class="total-time">' 
+            + renderTime(start, now) 
+            + '</div>';
+        display.innerHTML = html;
+        
     }
 
     interval = setInterval(tick, 10);
+    split_delta = document.getElementById(`d0`);
+    split_timer =  document.getElementById(`id0`);
 }
 
 function stopStopwatch() {
@@ -96,31 +97,55 @@ function stopStopwatch() {
 }
 
 function splitTime() {
-    
-    
-    if(cursplit == L - 1) {
-        stopStopwatch();
-    }
     if(interval) {
         var now = new Date();
+        var local_splittime = renderSplitTime(start, now);
+        split_timer.innerHTML = local_splittime;
+        split_delta.innerHTML = renderDeltaTime(delta_time);
         
-        split_element[cursplit][1].innerHTML = "0";
-        //split_element[cursplit][1].innerHTML = renderTime(start, now);
-        // if (split == null) {
-        //     times.innerHTML += '<div class="split-time">' 
-        //         + renderTime(start, now)
-        //         + '</div>';
-        
-        //     times.style.display = 'block';
-        // } else {
-        //     times.innerHTML += '<div class="split-time">' 
-        //         + renderTime(split, now)
-        //         + '</div>';
-        // }
+        colorGen(split_delta);
+        colorGen(previous_time);
+
+        if(cursplit == L - 1) {
+            stopStopwatch();
+            return;
+        }
+        previous_time.innerHTML = renderDeltaTime((now - split) - (split_List[cursplit + 1][3] - split_List[cursplit][3]));
         cursplit++;
+        run_delta = delta_time;
+        pb = pb + split_List[cursplit][3];
+        
+        if(cursplit != L - 1) {
+            split_delta = document.getElementById(`d${cursplit}`);
+            split_timer =  document.getElementById(`id${cursplit}`);
+        }
+        else
+        {
+            split_delta = document.getElementById(`final_split_delta`)
+            split_timer = document.getElementById(`final_split_time`)           
+        }
+
         split = now;
     }
-    
+}
+
+function colorGen(e) {
+    if(delta_time < 0) {
+        if(delta_time > run_delta) {
+            e.setAttribute('style', 'color:lightgreen');
+        }
+        else {
+            e.setAttribute('style', 'color:green');
+        }
+    }
+    else {
+        if(delta_time < run_delta) {
+            e.setAttribute('style', 'color:lightcoral');
+        }
+        else {
+            e.setAttribute('style', 'color:red');
+        }
+    }
 }
 
 function calculatePeriod(t1, t2) {
@@ -169,13 +194,85 @@ function renderTime(t1, t2) {
     var text = '';
 
     if (period.hours) {
-        text += padLeft(period.hours, 2) + ':';
+        text += period.hours + ':'
+        text += padLeft(period.minutes, 2) + ':';
+        text += padLeft(period.seconds, 2) + '.';
+        text += padLeft(Math.round(period.milliseconds / 10), 2);
+        return text;
     }
     if (period.minutes) {
-        text += padLeft(period.minutes, 2) + ':';
+        text += period.minutes + ':';
+        text += padLeft(period.seconds, 2) + '.';
+        text += padLeft(Math.round(period.milliseconds / 10), 2);
+        return text;
     }   
-    text += padLeft(period.seconds, 2) + '.';
-    text += padLeft(Math.floor(period.milliseconds / 10), 2);
 
+    text += period.seconds + '.';
+    text += padLeft(Math.round(period.milliseconds / 10), 2);
+    return text;
+}
+
+function renderSplitTime(t1, t2) {
+    var period = calculatePeriod(t1, t2);
+    var text = '';
+
+    if (period.hours) {
+        text += padLeft(period.hours, 2) + ':';
+        text += padLeft(period.minutes, 2); 
+    }
+    else {
+        text += period.minutes 
+    }
+     
+    text += ':' + padLeft(period.seconds, 2)
+
+    return text;
+}
+
+function renderDeltaTime(t) {
+    if(t < 0) {
+        var text = "-"
+        t = t * -1;
+    }
+    else {
+        var text = ""
+    }
+    var seconds = t / 1000;
+    var hours = parseInt(seconds / 3600);
+    var minutes = parseInt(seconds/ 60);
+    seconds = parseInt(seconds % 60);
+    
+    if(hours) {
+        text += hours + ':'
+        text += padLeft(minutes, 2) + ':';
+        text += padLeft(seconds, 2);
+        return text;
+    }
+    if(minutes) {
+        text += minutes + ':';
+        text += padLeft(seconds, 2);
+        return text;
+    }
+
+    text += seconds + '.' + Math.round(t % 1000 / 100) % 10;
+    return text;
+}
+
+function toTime(t) {
+    var text = "";
+    var seconds = t / 1000;
+    var hours = parseInt(seconds / 3600);
+    var minutes = parseInt(seconds/ 60);
+    seconds = parseInt(seconds % 60);
+    
+    if(hours) {
+        text += hours + ':'
+        text += padLeft(minutes, 2) + ':';
+        text += padLeft(seconds, 2);
+    }
+    else {
+        text += minutes + ':'
+        text += padLeft(seconds, 2);
+    }
     return text;
 }
